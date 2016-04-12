329413f0cf486413f921aaca069f53a72ac82726
ZpeedX <saboo@kth.se>
2016-04-12 23:11:51 +0200

    fråga 6 klart

--- Inlupp 1/SQLQuery 5.sql
+++ Inlupp 1/SQLQuery 5.sql
@@ -1,9 +1,11 @@
-SELECT XMLELEMENT(NAME "Kurstillfälle",
+SELECT XMLELEMENT(NAME "Resultat", XMLAGG(ktillf))
+FROM
+(SELECT XMLELEMENT(NAME "Kurstillfälle",
 XMLATTRIBUTES(kurs.kurskod AS "kurskod", kurs.kursben AS "kursnamn", ktillf.sdat AS "startdat", ktillf.lokal AS "lokal"),
-XMLFOREST(lärare.lnamn AS "Lärare", count(deltag.elev) AS "AntalElever"))
+XMLFOREST(lärare.lnamn AS "Lärare", count(deltag.elev) AS "AntalElever")) as ktillf
 FROM kurs INNER JOIN
      ktillf ON kurs.kurskod = ktillf.kurs INNER JOIN
      deltag ON ktillf.sdat = deltag.sdat AND ktillf.kurs = deltag.kurs
 	 INNER JOIN lärare ON lärare.lid = ktillf.lärare
-	 GROUP BY kurs.kurskod, kursben, ktillf.sdat, ktillf.lokal, lärare.lnamn
+	 GROUP BY kurs.kurskod, kursben, ktillf.sdat, ktillf.lokal, lärare.lnamn)
 
--- Inlupp 1/SQLQuery 6.sql
+++ Inlupp 1/SQLQuery 6.sql
@@ -1,6 +1,10 @@
-SELECT e.
-FROM(
-SELECT XMLQUERY('for $a in $e
-return element Kurslängd {attribute antalveckor{$a//längd}}'
-PASSING Info AS "e") as e
-				FROM Kurs)
+SELECT XMLELEMENT(NAME"Resultat", XMLAGG(kurslängd))
+FROM
+(SELECT XMLELEMENT(NAME "Kurslängd", XMLATTRIBUTES(längder AS "antalveckor", (SUM(snitt)/COUNT(snitt)) as "snittpris")) AS kurslängd
+FROM
+				(
+				SELECT XMLCAST(XMLQUERY('$e//längd'
+PASSING Info AS "e") as INTEGER) as längder, XMLCAST(XMLQUERY('$e//pris'
+PASSING Info AS "e") as INTEGER) as snitt
+FROM Kurs)
+group by längder)
