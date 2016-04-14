8a8de7c7955a178a546d6a0b99bd486d4f0aa850
Evan Saboo <saboo@kth.se>
2016-04-14 19:50:12 +0200

    lite ändringar i fråga 4 in1

--- Inlupp 1/SQLQuery 4.sql
+++ Inlupp 1/SQLQuery 4.sql
@@ -1,12 +1,11 @@
 SELECT XMLAGG(XMLELEMENT(NAME "Resultat" , elevdata))
 FROM (SELECT XMLELEMENT(NAME "Elev", 
-XMLATTRIBUTES(enamn AS "namn"), 
+XMLATTRIBUTES(a.namn AS "namn"), 
 XMLAGG(XMLELEMENT(NAME "Lokal", a.lokal))) AS elevdata
-FROM elev, (SELECT DISTINCT ktillf.lokal, elev.enamn AS namn FROM 
+FROM (SELECT DISTINCT ktillf.lokal, elev.enamn AS namn FROM 
 elev, deltag, ktillf
 WHERE 
-deltag.sdat = ktillf.sdat AND deltag.kurs = ktillf.kurs AND elev.eid = deltag.elev) AS a
-WHERE XMLEXISTS('$K//adress[@postort = "Kista"]'
-				PASSING Kontakt as "K")
-				    AND a.namn = elev.enamn
-				    group by enamn)
+deltag.sdat = ktillf.sdat AND deltag.kurs = ktillf.kurs AND elev.eid = deltag.elev
+AND XMLEXISTS('$K//adress[@postort = "Kista"]'
+				PASSING Kontakt as "K")) as a
+				    group by namn)
